483ef79b9652609603fac9f877f47a7300a1f209
Tanzimul Islam <53827851+tanzim721@users.noreply.github.com>
2024-01-12 01:14:10 +0600

    frontend

--- resources/views/frontend/aop/view.blade.php
+++ resources/views/frontend/aop/view.blade.php
@@ -5,15 +5,30 @@
 
 @section('content')
    
-    
-
-    <!-- Start Products  -->
-    <div class="products-box">
+    <!-- Start All Title Box -->
+    <div class="all-title-box">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
+                    <h2><strong>AOP</strong></h2>
+                    <ul class="breadcrumb">
+                        <strong>
+                            <li class="breadcrumb-item"><a href="{{route('home1')}}">Home</a></li>
+                            <li class="breadcrumb-item active">AOP</li>
+                        </strong>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Start Products  -->
+    <div class="products-box">
+        <div class="container contact-info-left">
+            <div class="row">
+                <div class="col-lg-12">
                     <div class="title-all text-center">
-                        <h1 class="text-danger"><strong>Our Team</strong></h1>
+                        <h1 class="text-danger"><strong>AOP</strong></h1>
                     </div>
                 </div>
             </div>
@@ -22,71 +37,71 @@
                     <div class="special-menu text-center">
                         <div class="button-group filter-button-group">
                             <button class="active" data-filter="*">All</button>
-                            <button data-filter=".supplierIdentifier">Top featured</button>
-                            <button data-filter=".merchandising">Merchandising</button>
-                            <button data-filter=".compititivePricing">Compititive Pricing</button>
+                            <button data-filter=".one">AOP one</button>
+                            <button data-filter=".two">AOP two</button>
+                            <button data-filter=".three">AOP three</button>
                         </div>
                     </div>
                 </div>
             </div>
 
             <div class="row special-list">
-                @foreach($merchandisings as $merchandising)
-                <div class="col-lg-4 col-md-6 special-grid merchandising">
+                @foreach($AopOnes as $AopOnes)
+                <div class="col-lg-4 col-md-6 special-grid one">
                     <div class="products-single fix">
                         <div class="box-img-hover">
                             <div class="type-lb">
-                                <p class="sale">Merchandising</p>
+                                <p class="sale">AOP one</p>
                             </div>
-                            <img src="{{asset('upload/merchandising_images/'.$merchandising->image)}}" class="img-fluid" alt="Image" style="width:100%; height:300px">
+                            <img src="{{asset('upload/aop_ones_images/'.$AopOnes->image)}}" class="img-fluid" alt="Image" style="width:100%; height:300px">
                             <div class="mask-icon">
                                 <ul>
-                                    <li><a href="{{route('merchandising')}}" data-toggle="tooltip" data-placement="right" title="View"><i class="fas fa-eye"></i></a></li>
+                                    <li><a href="" data-toggle="tooltip" data-placement="right" title="View"><i class="fas fa-eye"></i></a></li>
                                     <!-- <li><a href="#" data-toggle="tooltip" data-placement="right" title="Compare"><i class="fas fa-sync-alt"></i></a></li>
                                     <li><a href="#" data-toggle="tooltip" data-placement="right" title="Add to Wishlist"><i class="far fa-heart"></i></a></li> -->
                                 </ul>
-                                <p class="text-light pt-5 px-2">{{$merchandising->long_title}}</p>
+                                <p class="text-light pt-5 px-2">{{$AopOnes->short_title}}</p>
                             </div>
                         </div>
                         
                     </div>
                 </div>
                 @endforeach
-                @foreach($supplierIdentifiers as $supplierIdentifier)
-                <div class="col-lg-4 col-md-6 special-grid supplierIdentifier">
+                @foreach($AopTwos as $AopTwo)
+                <div class="col-lg-4 col-md-6 special-grid two">
                     <div class="products-single fix">
                         <div class="box-img-hover">
                             <div class="type-lb">
-                                <p class="new">Supplier Identifier</p>
+                                <p class="new">AOP Two</p>
                             </div>
-                            <img src="{{asset('upload/supplier_identifiers_images/'.$supplierIdentifier->image)}}" class="img-fluid" alt="Image" style="width:100%; height:300px">
+                            <img src="{{asset('upload/aop_twos_images/'.$AopTwo->image)}}" class="img-fluid" alt="Image" style="width:100%; height:300px">
                             <div class="mask-icon">
                                 <ul>
-                                    <li><a href="{{route('supplier_identifier')}}" data-toggle="tooltip" data-placement="right" title="View"><i class="fas fa-eye"></i></a></li>
+                                    <li><a href="" data-toggle="tooltip" data-placement="right" title="View"><i class="fas fa-eye"></i></a></li>
                                     <!-- <li><a href="#" data-toggle="tooltip" data-placement="right" title="Compare"><i class="fas fa-sync-alt"></i></a></li>
                                     <li><a href="#" data-toggle="tooltip" data-placement="right" title="Add to Wishlist"><i class="far fa-heart"></i></a></li> -->
                                 </ul>
-                                <p class="text-light pt-5 px-2">{{$supplierIdentifier->long_title}}</p>
+                                <p class="text-light pt-5 px-2">{{$AopTwo->short_title}}</p>
                             </div>
                         </div>
                     </div>
                 </div>
                 @endforeach
-                @foreach($compititivePricings as $compititivePricing)
-                <div class="col-lg-4 col-md-6 special-grid compititivePricing">
+                @foreach($AopThrees as $AopThree)
+                <div class="col-lg-4 col-md-6 special-grid three">
                     <div class="products-single fix">
                         <div class="box-img-hover">
                             <div class="type-lb">
-                                <p class="sale">Compititive Pricings</p>
+                                <p class="sale">AOP Three</p>
                             </div>
-                            <img src="{{asset('upload/compititive_pricings_images/'.$compititivePricing->image)}}" class="img-fluid" alt="Image" style="width:100%; height:300px">
+                            <img src="{{asset('upload/aop_threes_images/'.$AopThree->image)}}" class="img-fluid" alt="Image" style="width:100%; height:300px">
                             <div class="mask-icon">
                                 <ul>
-                                    <li><a href="{{route('compititive_pricing')}}" data-toggle="tooltip" data-placement="right" title="View"><i class="fas fa-eye"></i></a></li>
+                                    <li><a href="" data-toggle="tooltip" data-placement="right" title="View"><i class="fas fa-eye"></i></a></li>
                                     <!-- <li><a href="#" data-toggle="tooltip" data-placement="right" title="Compare"><i class="fas fa-sync-alt"></i></a></li>
                                     <li><a href="#" data-toggle="tooltip" data-placement="right" title="Add to Wishlist"><i class="far fa-heart"></i></a></li> -->
                                 </ul>
-                                <p class="text-light pt-5 px-2">{{$compititivePricing->long_title}}</p>
+                                <p class="text-light pt-5 px-2">{{$AopThree->short_title}}</p>
                             </div>
                         </div>
                         
